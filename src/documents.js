5571a925eaa4dff32866cbd86c7b555a5d4ef889
jchaselubitz <jchaselubitz@gmail.com>
2019-03-05 20:47:29 +0000

    moved editing functionality from the form to each individual item.

--- src/documents.js
+++ src/documents.js
@@ -1,22 +1,5 @@
 
 // ===================== Presentation =======================
-
-function showDoc (doc) {
-  const docCard = document.createElement('div')
-  console.log(doc.workspace_id)
-  const docTitle = doc.name
-  const status = doc.paid
-  const docDeadline = doc.deadline
-  docCard.innerHTML =
-  `<h2 class="docTitle">${docTitle}</h2>
-  <p class="paid">${status}</p>
-  <p class="deadline">Created on: ${docDeadline}</p>
-  <button>Edit</button>
-  <button>Delete</button>`
-  workspaceEl.append(docCard)
-  setEditEvent(docCard, doc)
-}
-
 function showAllDocs (currentWorkspace) {
   getDocuments()
     .then(resp => {
@@ -28,11 +11,85 @@ function showAllDocs (currentWorkspace) {
     })
 }
 
-// ===================== Creation =======================
+function showDoc (doc) {
+  const docCard = document.createElement('div')
+  docCard.innerHTML =
+  `<h2 class="title">${doc.name}</h2>
+  <p class="paid">${doc.paid}</p>
+  <p class="deadline">Deadline: ${doc.deadline}</p>
+  <button>Delete</button>`
+  setEditTitle(docCard, doc)
+  setEditStatus(docCard, doc)
+  setEditDeadline(docCard, doc)
+  workspaceEl.append(docCard)
+}
 
-// button for showing a form
-// form with fields upload, title, status, duedate
-// hidden field for workspace
+// ===================== Editing =======================
+
+function setEditTitle (docCard, doc) {
+  return setEditEvent(docCard, doc, 'title')
+}
+function setEditStatus (docCard, doc) {
+  return setEditEvent(docCard, doc, 'paid')
+}
+function setEditDeadline (docCard, doc) {
+  return setEditEvent(docCard, doc, 'deadline')
+}
+
+function setEditEvent (docCard, doc, classname) {
+  docCard.addEventListener('click', (event) => {
+    if (event.target.className === classname) {
+      createEditField(docCard, doc, classname)
+    }
+  })
+}
+
+function createEditField (docCard, doc, classname) {
+  let fieldLocation = docCard.querySelector(`.${classname}`)
+  const formEl = document.createElement('form')
+  if (classname === "title") {
+    formEl.innerHTML = `Title: <input type="text" name="title">
+    <input type="submit" value="Submit">`
+    titleSubmission (formEl, doc)
+  } else if (classname === "paid") {
+    formEl.innerHTML = `Paid: <input type="checkbox" name="paid" ${doc.paid === true ? 'checked' : 'unchecked'}>
+    <input type="submit" value="Submit">`
+    paidSubmission (formEl, doc)
+  } else if (classname === "deadline") {
+    formEl.innerHTML = `Deadline: <input type="date" name="deadline">
+    <input type="submit" value="Submit">`
+    deadlineSubmission (formEl, doc)
+  }
+  fieldLocation.append(formEl)
+}
+
+function titleSubmission (formEl, doc) {
+  return submitEdit(formEl, doc, "title")
+}
+
+function paidSubmission (formEl, doc) {
+  return submitEdit(formEl, doc, "paid")
+}
+
+function deadlineSubmission (formEl, doc) {
+  return submitEdit(formEl, doc, "deadline")
+}
+
+function submitEdit(formEl, doc, sender) {
+  formEl.addEventListener('submit', (event) => {
+    event.preventDefault()
+    updateDocument({
+      name: sender === "title" ? event.target.title.value : doc.name,
+      paid: sender === "paid" ? event.target.paid.checked : doc.paid,
+      deadline: sender === "deadline" ? event.target.deadline.value : doc.deadline,
+      workspace_id: doc.workspace_id,
+      doctext: doc.doctext,
+      id: doc.id
+    })
+  })
+}
+
+// ===================== Creation =======================
 
 newDocButton = document.createElement('button')
 newDocButton.innerText = 'Add document'
@@ -42,22 +99,17 @@ function setCreateEvent () {
     createNewForm()
   })
   workspaceEl.append(newDocButton)
-  
 }
 setCreateEvent()
 
 function createNewForm () {
   const FormEl = document.createElement('form')
-  FormEl.innerHTML =
-  `
+  FormEl.innerHTML = `
   <input type="hidden" name="workspace" value="${currentWorkspace.id}">
   <input type="hidden" name="docText" value=""> 
-  Document name:
-  <input type="text" name="name"><br>
-  Paid:
-  <input type="checkbox" name="paid"><br>
-  Deadline:
-  <input type="date" name="deadline"><br>
+  Document name: <input type="text" name="name"><br>
+  Paid: <input type="checkbox" name="paid"><br>
+  Deadline: <input type="date" name="deadline"><br>
   <input type="submit" value="Submit">`
   FormEl.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -66,49 +118,11 @@ function createNewForm () {
       paid: event.target.paid.checked,
       deadline: event.target.deadline.value,
       workspace_id: event.target.workspace.value,
-      doctext: event.target.docText.value,
+      doctext: event.target.docText.value
 
     })
   })
   workspaceEl.append(FormEl)
-}
-// ===================== Editing =======================
-
-function setEditEvent (docCard, doc) {
-  docCard.addEventListener('click', (event) => {
-    if (event.target.innerText === 'Edit') {
-      createEditForm(docCard, doc)
-    }
-  })
-}
-
-function createEditForm (docCard, doc) {
-  const editFormEl = document.createElement('form')
-  editFormEl.innerHTML =
-  `
-  <input type="hidden" name="docId" value="${doc.id}">
-  <input type="hidden" name="workspace" value="${doc.workspace_id}">
-  <input type="hidden" name="docText" value=""> 
-  Document name:
-  <input type="text" name="name"><br>
-  Paid:
-  <input type="checkbox" name="paid" ${doc.paid === true ? 'checked' : 'unchecked'}><br>
-  Deadline:
-  <input type="date" name="deadline"><br>
-  <input type="submit" value="Submit">`
-  editFormEl.addEventListener('submit', (event) => {
-    event.preventDefault()
-    updateDocument({
-      name: event.target.name.value,
-      paid: event.target.paid.checked,
-      deadline: event.target.deadline.value,
-      workspace_id: event.target.workspace.value,
-      doctext: event.target.docText.value,
-      id: event.target.docId.value
-
-    })
-  })
-  docCard.append(editFormEl)
 }
 
 // ===================== Deleting =======================
